4be0c022ade0769dc5e90e9fa9d9788d1358d59c
Sarah Healey <s.deane.healey@gmail.com>
2017-03-29 08:15:40 -0600

    added foreign key linking wine notes and user tables

---
+++ migrations/20170329080534_foreign_key_linking_wine_notes_and_users.js
@@ -0,0 +1,18 @@
+
+exports.up = function(knex, Promise) {
+    return knex.schema.table('wine_notes', (wine_notes) => {
+        wine_notes.integer('users_id').references('users.id').notNullable().onDelete('CASCADE');
+    });
+};
+
+exports.down = function(knex, Promise) {
+		return knex.schema.table('wine_notes', function(wine_notes) {
+			wine_notes.dropColumn('users_id');
+	});
+};
+
+
+
+
+
+
