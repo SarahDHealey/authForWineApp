3f50b572239b8460e7149ebe116e48c2955b41eb
Sarah Healey <s.deane.healey@gmail.com>
2017-05-07 11:19:20 -0600

    working on route to select all wines from database

--- router.js
+++ router.js
@@ -5,10 +5,20 @@ const passport = require('passport');
 const requireAuth = passport.authenticate('jwt', { session: false });
 const requireSignin = passport.authenticate('local', { session: false });
 
+const knex = require('./db/knex');
+
 module.exports = function(app) {
 
   app.get('/', requireAuth, function(req, res) {
     res.send({hi: 'There!'});
+  });
+
+  app.get('/wine_profile/:id', requireAuth, function(req, res) {
+    knex('wine')
+    .select()
+    .then(wines => {
+    res.send({wines: wines});
+    })
   });
 
   app.post('/signin', requireSignin, Authentication.signin)
